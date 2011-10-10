6c25d2afc6792e9db4331723a6a22a2678039fd4
Kevin Mehall <km@kevinmehall.net>
2011-10-10 15:50:54 -0400

    firmware/update.py: don't print error message when the device is already in bootloader mode

--- firmware/update.py
+++ firmware/update.py
@@ -10,11 +10,13 @@ from flash import *
 try:
 	b = Bootloader()
 	b.handle_args(sys.argv)
+	exit(0)
 except AttributeError:
 	usb.core.find(idVendor=0x9999, idProduct=0xffff).ctrl_transfer(0x40|0x80, 0xBB, 0, 0, 1)
 	time.sleep(1)
 	b = Bootloader()
 	b.handle_args(sys.argv)
-else:
-	print "can't find CEE or xmega in bootloader mode"
-	exit()
+	exit(0)
+
+print "can't find CEE or xmega in bootloader mode"
+exit(2)
